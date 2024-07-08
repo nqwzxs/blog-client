007a73269cc4248a8993daf29ce4d11d0ac8184b
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2024-07-08 22:46:23 +0500

    add post page

---
+++ src/routes/Post.jsx
@@ -0,0 +1,76 @@
+import { useLoaderData } from 'react-router-dom'
+import { formatDistance } from 'date-fns'
+import Comments from '../components/Comments'
+
+export async function action({ params, request }) {
+  try {
+    const formData = await request.formData()
+    const data = {
+      author: formData.get('author'),
+      body: formData.get('body'),
+    }
+
+    const response = await fetch(
+      `http://localhost:3000/api/v1/posts/${params.postId}/comments`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }
+    )
+    return response
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export async function loader({ params }) {
+  const postResponse = await fetch(
+    `http://localhost:3000/api/v1/posts/${params.postId}`
+  )
+
+  if (!postResponse.ok) {
+    throw new Response('', {
+      status: postResponse.status,
+      statusText: postResponse.statusText,
+    })
+  }
+
+  const commentsResponse = await fetch(
+    `http://localhost:3000/api/v1/posts/${params.postId}/comments`
+  )
+
+  const post = await postResponse.json()
+  const comments = await commentsResponse.json()
+
+  return { post, comments }
+}
+
+function Post() {
+  const { post, comments } = useLoaderData()
+
+  // const formattedDate = formatDistance(
+  //   new Date(post.date_created),
+  //   new Date(),
+  //   {
+  //     addSuffix: true,
+  //   }
+  // )
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-4xl spacing">{post.title}</h1>
+        {/* <span className="text-gray-500">{formattedDate}</span> */}
+      </div>
+      <p className="whitespace-pre-line text-justify pb-8 border-b-2 border-gray-200">
+        {post.body}
+      </p>
+      <Comments comments={comments} />
+    </div>
+  )
+}
+
+export default Post
