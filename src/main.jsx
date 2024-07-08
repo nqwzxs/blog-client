e0c8f55916abb75cf50fea649056b7ba3fbba3c4
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2024-07-08 22:46:47 +0500

    add main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './main.css'
+import App from './routes/App.jsx'
+import ErrorPage from './routes/ErrorPage.jsx'
+import Home, { loader as homeLoader } from './routes/Home.jsx'
+import Post, {
+  loader as postLoader,
+  action as postAction,
+} from './routes/Post.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        loader: homeLoader,
+      },
+      {
+        path: 'posts/:postId',
+        element: <Post />,
+        loader: postLoader,
+        action: postAction,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
